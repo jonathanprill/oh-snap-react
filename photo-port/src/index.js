b916b3880c4064ee08a919066d99cfdac19efa2f
jonyprill <jonathantprill@gmail.com>
2022-05-01 16:21:19 -0500

    initial push

---
+++ photo-port/src/index.js
@@ -0,0 +1,18 @@
+// Think of the index.js file as the main entry point for the application
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
